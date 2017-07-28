d3540fc8c2a38309e98e4e72a4f0d69227997105
DSnimshchikov <Dmitriy.Snimshchikov@homecredit.ru>
2017-07-28 04:09:52 +0300

    refactoring

--- src/actions/chooseBestSolution.js
+++
@@ -1,7 +0,0 @@
-import { CHOOSE_BEST_SOLUTION } from './const';
-
-function action(parameter) {
-  return { type: CHOOSE_BEST_SOLUTION, parameter };
-}
-
-module.exports = action;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
-import chooseBestSolution from '../actions/chooseBestSolution.js';
 import {filterProducts, fetchProductSuccess, loadProducts} from './filter-action.js';
 
 const actions = {
-  filterProducts, fetchProductSuccess, loadProducts,
-  chooseBestSolution
+  filterProducts, fetchProductSuccess, loadProducts
 };
 
 module.exports = actions;
